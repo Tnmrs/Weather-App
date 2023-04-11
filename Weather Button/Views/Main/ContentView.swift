b9a5d8446c23a581f9bdde64ec5fa84a5f45898d
tnmrs <promomyers@gmail.com>
2023-04-11 22:20:15 +0300

    Initial Commit

---
+++ Weather Button/Views/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Weather Button
+//
+//  Created by Дмитрий Сенаторов on 11.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather Button/Weather_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ButtonApp.swift
+//  Weather Button
+//
+//  Created by Дмитрий Сенаторов on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
